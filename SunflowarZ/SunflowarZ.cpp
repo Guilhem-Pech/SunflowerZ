b73aaf7a28632ae1614665ee4958aebbd2d8117b
Aloïs CARREZ <alois.carrez.auditeur@lecnam.net>
2019-09-23 16:33:16 +0200

    :sparkles: Map

--- SunflowarZ/SunflowarZ.cpp
+++ SunflowarZ/SunflowarZ.cpp
@@ -2,30 +2,50 @@
 
 #include <Windows.h>
 #include <iostream>
-#define SCREEN_WIDTH 500
-#define SCREEN_HEIGHT 500
+#define SCREEN_WIDTH 100
+#define SCREEN_HEIGHT 40
+COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
+COORD dwBufferCoord = { 0, 0 };
+HANDLE hOutput;
+
+void calcMapZ(CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH]) {
+	for (int x = 0; x < SCREEN_WIDTH; ++x) {
+		for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+			buffer[y][x].Attributes = 0x00b0;
+		}
+	}
+
+	for (int y = 5; y <= (SCREEN_HEIGHT - 1); ++y) {
+		int a = y / 11;
+		for (int x = 0; x <= y + a * a && x <= (SCREEN_WIDTH / 2); ++x) {
+			buffer[y][x].Attributes = 0x0080;
+			buffer[y][SCREEN_WIDTH - 1 - x].Attributes = 0x0080;
+		}
+	}
+
+}
+
+void calcSunflowerZ(CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH]) {
+	//TODO
+}
+
 
 int main()
 {
-	HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
+	hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
 
-	COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
-	COORD dwBufferCoord = { 0, 0 };
 	SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
 
-	CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
 
-	ReadConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
-		dwBufferCoord, &rcRegion);
+	CHAR_INFO MapZ[SCREEN_HEIGHT][SCREEN_WIDTH] = { 0 };
 
-	buffer[5][10].Char.AsciiChar = 'H';
-	buffer[5][10].Attributes = 0x0E;
-	buffer[5][11].Char.AsciiChar = 'i';
-	buffer[5][11].Attributes = 0x0B;
-	buffer[5][12].Char.AsciiChar = '!';
-	buffer[5][12].Attributes = 0x0A;
+	ReadConsoleOutput(hOutput, (CHAR_INFO *)MapZ, dwBufferSize,dwBufferCoord, &rcRegion);
+	
+	calcMapZ(MapZ);
+	
 
-	WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
-		dwBufferCoord, &rcRegion);	
+	while (1) {
+		calcSunflowerZ(MapZ);
+		WriteConsoleOutput(hOutput, (CHAR_INFO *)MapZ, dwBufferSize, dwBufferCoord, &rcRegion);
+	}
 }
-
