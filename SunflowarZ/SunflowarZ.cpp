7345a8afff3bed35e9984a572bdbb49d77d0d373
Guilhem PECH <guilhem.pech@gmail.com>
2019-09-23 13:48:28 +0200

    :hankey: Bizarre

--- SunflowarZ/SunflowarZ.cpp
+++ SunflowarZ/SunflowarZ.cpp
@@ -2,10 +2,30 @@
 
 #include <Windows.h>
 #include <iostream>
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
 
 int main()
 {
+	HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
 
-    std::cout << "Hello Worldz!\n";
+	COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
+	COORD dwBufferCoord = { 0, 0 };
+	SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
+
+	CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+
+	ReadConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
+		dwBufferCoord, &rcRegion);
+
+	buffer[5][10].Char.AsciiChar = 'H';
+	buffer[5][10].Attributes = 0x0E;
+	buffer[5][11].Char.AsciiChar = 'i';
+	buffer[5][11].Attributes = 0x0B;
+	buffer[5][12].Char.AsciiChar = '!';
+	buffer[5][12].Attributes = 0x0A;
+
+	WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
+		dwBufferCoord, &rcRegion);	
 }
 
